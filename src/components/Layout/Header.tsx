2c59428a48d3af933f0734a54271df44851d8ce7
zzaishu49-tech <zzaishu49@gmail.com>
2025-09-18 22:16:13 +0530

    Updated package-lock.json

--- src/components/Layout/Header.tsx
+++
@@ -1,5 +0,0 @@
-@@ .. @@
- import React from 'react';
--import { useAuth } from '../../context/AuthContext';
-+import { useAuth } from '../../context/SupabaseAuthContext';
- import { LogOut, User } from 'lucide-react';
